9ef359ebfcdb7083ab0a74c5e7b0cb91bbbc7f4f
Sahilu <Wmsm16@gmail.com>
2026-09-13 11:05:37 +0300

    Downloaded all the dependecies, checked both and the backend, and currently working on the database

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Task Management API is running" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
